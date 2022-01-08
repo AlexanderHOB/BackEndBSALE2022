c85334ee20ae8d87c738cf334d7868ddf3dfa441
AlexanderHOB <obispoa1@gmail.com>
2022-01-08 06:52:56 -0500

    Add Filter Data Function in ApiResponser

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -14,9 +14,24 @@ trait ApiResponser
         if ( $collection->isEmpty()){
             return $this->successResponse(['data'=>$collection],$code);
         }
+        $collection = $this->filterData($collection);
         return $this->successResponse($collection,$code);
     }
+    //function to return a One Resource
     protected function showOne(Model $instance, $code = 200) {
         return $this->successResponse($instance,$code);
     }
+    //function to filterData by querys
+    protected function filterData(Collection $collection){
+        foreach(request()->query as $query =>$value){
+            if(isset($query,$value)){
+                $collection = $collection->filter(function ($query) use ($value) {
+                    return strpos($query->name, $value) !== false;
+                });
+            }
+        }
+
+        return $collection;
+    }
+
 }
