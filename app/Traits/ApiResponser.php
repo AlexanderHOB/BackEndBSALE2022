d3a97d91214d010dc071fc16452abfd2ec7e33c0
AlexanderHOB <obispoa1@gmail.com>
2022-01-08 07:28:20 -0500

    Add Sort Data Function in ApiResponser

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -15,6 +15,7 @@ trait ApiResponser
             return $this->successResponse(['data'=>$collection],$code);
         }
         $collection = $this->filterData($collection);
+        $collection = $this->sortData($collection);
         return $this->successResponse($collection,$code);
     }
     //function to return a One Resource
@@ -24,10 +25,27 @@ trait ApiResponser
     //function to filterData by querys
     protected function filterData(Collection $collection){
         foreach(request()->query as $query =>$value){
-            if(isset($query,$value)){
+            if(isset($query,$value) && $query !== 'sort_by' && $query !== 'sort'){
                 $collection = $collection->filter(function ($query) use ($value) {
                     return strpos($query->name, $value) !== false;
                 });
+            }
+        }
+        return $collection;
+    }
+    //function to sort data by attribute
+    protected function sortData(Collection $collection){
+        // validate that request has sort by attribute
+        if(request()->has('sort_by')){
+            $attribute = request()->sort_by;
+            $collection = $collection->sortBy->{$attribute};
+            
+            //validate if the request has a sort order by attribute
+            if(request()->has('sort') &&  in_array(request()->has('sort'),['asc','desc'])){
+                $order = request()->sort;
+                $collection = ($order === 'desc') 
+                                ? $collection->sortByDesc->{$attribute}
+                                : $collection->sortBy->{$attribute};
             }
         }
 
