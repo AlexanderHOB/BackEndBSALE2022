f1bb3fc0e11bd9f064109a4a08ba05bb1ad7e3d7
AlexanderHOB <obispoa1@gmail.com>
2022-01-08 07:46:18 -0500

    Add Pagination Function in ApiResponser

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -2,7 +2,8 @@
 namespace App\Traits;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Pagination\LengthAwarePaginator;
 trait ApiResponser
 {
     //function to return success response in JSON
@@ -16,6 +17,7 @@ trait ApiResponser
         }
         $collection = $this->filterData($collection);
         $collection = $this->sortData($collection);
+        $collection = $this->paginate($collection);
         return $this->successResponse($collection,$code);
     }
     //function to return a One Resource
@@ -25,7 +27,7 @@ trait ApiResponser
     //function to filterData by querys
     protected function filterData(Collection $collection){
         foreach(request()->query as $query =>$value){
-            if(isset($query,$value) && $query !== 'sort_by' && $query !== 'sort'){
+            if(isset($query,$value) && !in_array($query, ['sort_by','sort','per_page','page'])){
                 $collection = $collection->filter(function ($query) use ($value) {
                     return strpos($query->name, $value) !== false;
                 });
@@ -39,7 +41,7 @@ trait ApiResponser
         if(request()->has('sort_by')){
             $attribute = request()->sort_by;
             $collection = $collection->sortBy->{$attribute};
-            
+
             //validate if the request has a sort order by attribute
             if(request()->has('sort') &&  in_array(request()->has('sort'),['asc','desc'])){
                 $order = request()->sort;
@@ -48,8 +50,29 @@ trait ApiResponser
                                 : $collection->sortBy->{$attribute};
             }
         }
-
         return $collection;
     }
+    // function to paginate data by quantity
+    protected function paginate(Collection $collection){
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+        Validator::Validate(request()->all(),$rules);
+        //resolve current page
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage=15; //quantity  elements default
 
+        if(request()->has('per_page')){
+            $perPage = (int) request()->per_page;
+        }
+
+        $results = $collection->slice(($page-1)*$perPage,$perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results,$collection->count(),$perPage,$page,[
+            'path'=>LengthAwarePaginator::resolveCurrentPath()
+            ]);
+        $paginated->appends(request()->all());
+
+        return $paginated;
+    }
 }
